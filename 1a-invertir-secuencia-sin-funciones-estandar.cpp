da5fb55d77d9a6acc9abb27cb24d5e37a06b6bd4
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-10-30 19:45:34 +0100

    Create 1a-invertir-secuencia-sin-funciones-estandar.cpp

---
+++ 1a-invertir-secuencia-sin-funciones-estandar.cpp
@@ -0,0 +1,54 @@
+﻿/******************************************************************************\
+ * Curso de Programación 1. Tema 9
+ * Autores: Javier Martínez y Miguel Ángel Latre
+ * Última revisión: 30 de octubre de 2020
+ * Resumen: Solución al problema 1.º planteado en la clase de problemas
+ *          del tema 9 (vectores) que no utiliza funciones auxiliares.
+ *          Esta versión sigue rigurosamente el estándar de C++ en cuanto a la
+ *          declaración de vectores.
+ * Nota: El programa completo está contenido en este fichero, por lo que puede 
+ *       compilarse y ejecutarse con la extensión Run Code de Visual Studio 
+ *       Code, tal y como se hacía en las prácticas 1 y 2.
+\******************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Número máximo de enteros con los que trabajaremos
+ */
+const int DIMENSION_MAXIMA = 40;
+
+/*
+ * Programa que solicita al usuario, en primer lugar, un número positivo «n»,
+ * luego solicita «n» datos de tipo real y por último los escribe en la pantalla
+ * en orden inverso al introducido.
+ */
+int main() {
+    cout << "Introduzca un número positivo: ";
+    int n;
+    cin >> n;
+
+    if (n > DIMENSION_MAXIMA) {
+        n = DIMENSION_MAXIMA;
+    }
+
+    /* En esta versión, el dimensionamiento del vector se hace a partir de un 
+     * dato constante conocido en tiempo de compilación, de acuerdo con el 
+     * estándar de C++. Se va a trabajar con un vector «sobredimensionado»: de 
+     * las «DIMENSION_MAXIMA» que tiene el vector «datos», se va a trabajar solo 
+     * con las «n» primeras. */
+    double datos[DIMENSION_MAXIMA];
+
+    cout << "Introduzca " << n << " enteros: ";
+    for (int i = 0; i < n; i++) {
+        cin >> datos[i];
+    }
+
+    cout << "La secuencia en orden inverso es: " << endl;
+    for (int i = n - 1; i > 0; i--) {
+        cout << datos[i] << ", ";
+    }
+    cout << datos[0];
+
+    return 0;
+}
