3f22bcb77d4be75652a89791e65254241b938947
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-10-30 20:29:34 +0100

    Solución al problema 3b

---
+++ datos-pruebas.hpp
@@ -0,0 +1,15 @@
+/******************************************************************************\
+ * Curso de Programación 1. Tema 9
+ * Autores: Javier Martínez y Miguel Ángel Latre
+ * Última revisión: 30 de octubre de 2020
+ * Resumen: Datos comunes para las pruebas de las funciones «numPositivos»,
+ *          «copiar», «sonIguales» y «copiaPositivos».
+\******************************************************************************/
+
+const int VECTOR_0[] = {};
+const int VECTOR_1[] = {5};
+const int VECTOR_2[] = {-5};
+const int VECTOR_3[] = {-1, 1};
+const int VECTOR_4[] = {1, -1};
+const int VECTOR_5[] = {1, -2, 3, -4, 5, -6};
+const int VECTOR_6[] = {-1, 2, -3, 4, -5, 6};
