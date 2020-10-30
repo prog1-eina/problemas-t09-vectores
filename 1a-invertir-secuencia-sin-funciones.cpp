297635a57879a9035f569a203ed76b538af90e89
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-10-30 19:38:37 +0100

    Create 1a-invertir-secuencia-sin-funciones.cpp

---
+++ 1a-invertir-secuencia-sin-funciones.cpp
@@ -0,0 +1,40 @@
+﻿/******************************************************************************\
+ * Curso de Programación 1. Tema 9
+ * Autores: Javier Martínez y Miguel Ángel Latre
+ * Última revisión: 30 de octubre de 2020
+ * Resumen: Solución al problema 1.º planteado en la clase de problemas
+ *          del tema 9 (vectores) que no utiliza funciones auxiliares.
+ * Nota: El programa completo está contenido en este fichero, por lo que puede 
+ *       compilarse y ejecutarse con la extensión Run Code de Visual Studio 
+ *       Code, tal y como se hacía en las prácticas 1 y 2.
+\******************************************************************************/
+#include <iostream>
+using namespace std;
+
+
+/*
+ * Programa que solicita al usuario, en primer lugar, un número positivo «n»,
+ * luego solicita «n» datos de tipo real y por último los escribe en la pantalla
+ * en orden inverso al introducido.
+ */
+int main() {
+    cout << "Introduzca un número positivo: " << flush;
+    int n;
+    cin >> n;
+
+    double datos[n];
+    /* Advertencia: el dimensionamiento de un vector a partir de un dato no
+     * constante no forma parte del estándar de C++, pero está implementado por
+     * los compiladores GNU GCC y MinGW. */
+
+    cout << "Introduzca " << n << " enteros: ";
+    for (int i = 0; i < n; i++) {
+        cin >> datos[i];
+    }
+
+    cout << "La secuencia en orden inverso es: " << endl;
+    for (int i = n - 1; i > 0; i--) {
+        cout << datos[i] << ", ";
+    }
+    cout << datos[0];
+}
